d7fd6959dafb5aa1bb93508d0f1c259ef1b6fe64
ehddbs4521 <ehddbs452100@naver.com>
2026-09-03 14:51:30 +0900

    refactor: isolate Redis workloads

--- src/main/java/pocketpaystore/pocketpay_core/common/config/RedissonConfig.java
+++ src/main/java/pocketpaystore/pocketpay_core/common/config/RedissonConfig.java
@@ -4,6 +4,7 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,6 +12,12 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import io.lettuce.core.api.StatefulConnection;
 
 import java.time.Duration;
 
@@ -23,24 +30,71 @@ public class RedissonConfig {
 	@Value("${spring.data.redis.port}")
 	private int redisPort;
 
-	@Value("${spring.data.redis.ssl.enabled:false}")
+	@Value("${spring.data.redis.ssl.enabled}")
 	private boolean sslEnabled;
 
-	@Bean
+	@Value("${redis.clients.idempotency.max-active}")
+	private int idempotencyMaxActive;
+
+	@Value("${redis.clients.payment-event.max-active}")
+	private int paymentEventMaxActive;
+
+	@Value("${redis.clients.max-idle}")
+	private int maxIdle;
+
+	@Value("${redis.clients.min-idle}")
+	private int minIdle;
+
+	@Value("${redis.clients.max-wait}")
+	private Duration maxWait;
+
+	@Bean(name = {"idempotencyRedisConnectionFactory", "redisConnectionFactory"})
 	@Primary
-	public RedisConnectionFactory redisConnectionFactory() {
+	public RedisConnectionFactory idempotencyRedisConnectionFactory() {
+		return createPooledConnectionFactory(idempotencyMaxActive);
+	}
+
+	@Bean("paymentEventRedisConnectionFactory")
+	public RedisConnectionFactory paymentEventRedisConnectionFactory() {
+		return createPooledConnectionFactory(paymentEventMaxActive);
+	}
+
+	@Bean("idempotencyRedisTemplate")
+	@Primary
+	public StringRedisTemplate idempotencyRedisTemplate(
+			@Qualifier("idempotencyRedisConnectionFactory") RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	@Bean("paymentEventRedisTemplate")
+	public StringRedisTemplate paymentEventRedisTemplate(
+			@Qualifier("paymentEventRedisConnectionFactory") RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	private RedisConnectionFactory createPooledConnectionFactory(int maxActive) {
 		RedisStandaloneConfiguration standaloneConfig =
 			new RedisStandaloneConfiguration(redisHost, redisPort);
 
-		LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
-			LettuceClientConfiguration.builder()
-				.commandTimeout(Duration.ofSeconds(2));
+		GenericObjectPoolConfig<StatefulConnection<?, ?>> poolConfig = new GenericObjectPoolConfig<>();
+		poolConfig.setMaxTotal(maxActive);
+		poolConfig.setMaxIdle(Math.min(maxIdle, maxActive));
+		poolConfig.setMinIdle(Math.min(minIdle, maxActive));
+		poolConfig.setMaxWait(maxWait);
+
+		LettucePoolingClientConfiguration.LettucePoolingClientConfigurationBuilder builder =
+			LettucePoolingClientConfiguration.builder()
+				.commandTimeout(Duration.ofSeconds(2))
+				.poolConfig(poolConfig);
 
 		if (sslEnabled) {
 			builder.useSsl();
 		}
 
-		return new LettuceConnectionFactory(standaloneConfig, builder.build());
+		LettuceConnectionFactory connectionFactory =
+				new LettuceConnectionFactory(standaloneConfig, builder.build());
+		connectionFactory.setShareNativeConnection(false);
+		return connectionFactory;
 	}
 
 	@Bean(destroyMethod = "shutdown")
